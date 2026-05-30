1a211ab034b42222d33603a8a932a828bcd45969
María Paulina Ramírez Vásquez <maria.ramirez@zinobe.com>
2026-05-30 12:04:48 -0500

    Update qr-modal-data.blade.php

--- resources/views/components/ui/qr-modal-data.blade.php
+++ resources/views/components/ui/qr-modal-data.blade.php
@@ -60,8 +60,8 @@
                 <div class="bg-white p-8 rounded-3xl shadow-inner border border-secondary-200 dark:border-secondary-300">
                     <img src=""
                          :data-qr-url="url" 
-                         data-qr-color-light="#FCFCFC" 
-                         data-qr-color-dark="#1F1F1F"
+                         data-qr-color-light="#ffffff" 
+                         data-qr-color-dark="#ffffff"
                          alt="QR Code" 
                          class="w-64 h-64" />
                 </div>
