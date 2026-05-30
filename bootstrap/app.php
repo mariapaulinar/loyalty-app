7c9385ee3ac5b527636242d95c919fa83dfa38f2
María Paulina Ramírez Vásquez <maria.ramirez@zinobe.com>
2026-05-29 23:16:34 -0500

    Se agrega Sentry

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Support\Facades\File;
+use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -112,5 +113,6 @@ return Application::configure(basePath: dirname(__DIR__))
 
             app()->setLocale($locale);
         }
+        Integration::handles($exceptions);
     })
     ->create();
